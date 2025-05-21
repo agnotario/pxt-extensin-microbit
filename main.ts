fa5dd3174cbe6f71770445d2165c28666b763740
agnotario <agnotario@gmail.com>
2025-05-21 13:29:04 +0200

    Primera versión

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
-basic.forever(function () {
-	
-})
+/**
+ * Utilidades para el LED central del micro:bit
+ */
+//% weight=100 color=#0fbc11 icon="\uf0eb"
+namespace centroLed {
+    /**
+     * Enciende el LED central
+     */
+    //% block
+    export function encender(): void {
+        led.plot(2, 2)
+        console.log("LED central encendido")
+    }
+
+    /**
+     * Apaga el LED central
+     */
+    //% block
+    export function apagar(): void {
+        led.unplot(2, 2)
+        console.log("LED central apagado")
+    }
+}
